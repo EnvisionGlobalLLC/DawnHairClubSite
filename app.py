e0750a57bfa1183957212b1003691469a68f3c83
zeedestiny <46862598+zeedestiny@users.noreply.github.com>
2025-05-18 16:53:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def book():
 def contact():
     return render_template('contact.html')
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+#if __name__ == '__main__':
+    #app.run(host='0.0.0.0', port=5000, debug=True)
 
 
